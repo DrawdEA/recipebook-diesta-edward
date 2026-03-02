3b16c1dde24d73b8ef1ee7a7dbae195cdd794060
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2026-03-02 16:52:55 +0800

    feat: add reset password flow

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/recipes/list/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+# Print password reset emails to the console (no real email needed in development)
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
