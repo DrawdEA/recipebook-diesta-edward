295bc773c686cc8c616c20985484f6ecd4cd3523
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2026-03-12 23:46:19 +0800

    fix: pep8 styling

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -126,7 +127,7 @@ STATICFILES_DIRS = [
 STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
